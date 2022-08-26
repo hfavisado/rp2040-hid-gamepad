f42bd03f7f674150fb8bbd09c303ef4fb46c1466
Hal Amano <hal.f.avisado+github@gmail.com>
2022-08-27 06:52:27 +0900

    Add PicoGamepad from https://gitlab.com/realrobots/PicoGamepad

--- rp2040-hid-gamepad.ino
+++ rp2040-hid-gamepad.ino
@@ -1,38 +1,61 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-#define PIN       23
-#define NUMPIXELS 1
-#define GPIOLED   25
+#include "./PicoGamepad.h"
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 100
+PicoGamepad gamepad;
 
-void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  pixels.begin();
-  pinMode(GPIOLED,OUTPUT);
+// 16 bit integer for holding input values
+int val;
+
+void setup() {  
+  Serial.begin(115200);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // X Potentiometer on pin 26
+  pinMode(26, INPUT);
+  // Y Potentiometer on pin 27
+  pinMode(27, INPUT);
+
+  // Button on pin 
+  pinMode(28, INPUT_PULLUP);
 }
 
 void loop() {
-  pixels.clear();
-  int range = 3;
+
+  // Get input value from Pico analog pin
+  val = analogRead(26);
+
+  // Map analog 0-1023 value from pin to max HID range -32767 - 32767
+  val = map(val, 0, 1023, -32767, 32767);
+
+  // Send value to HID object
+  gamepad.SetX(val);
+
+  // Repeat with Y pin
+  val = analogRead(27);
+  val = map(val, 0, 1023, -32767, 32767);
+  gamepad.SetY(val);
   
-  for(int i=0; i<2; i++) {
-    int red = rand () % range;
-    int green = rand () % range;
-    int blue = rand () % range;
-    pixels.setPixelColor(0, pixels.Color(red, green, blue));
-    pixels.show();
-    if (i%2 == 0) {
-      digitalWrite(GPIOLED, LOW);
-    } else {
-      digitalWrite(GPIOLED, HIGH);
-    }
-    
-    delay(DELAYVAL);
-  }
+//  gamepad.SetZ(val);
+//  gamepad.SetRx(val);
+//  gamepad.SetRy(val);
+
+  gamepad.SetRz(val);
+  
+//  gamepad.SetS0(val);
+//  gamepad.SetS1(val);
+
+  // Set button 0 of 128 by reading button on digital pin 28
+  gamepad.SetButton(0, !digitalRead(28));
+
+  // Set hat direction, 4 hats available. direction is clockwise 0=N 1=NE 2=E 3=SE 4=S 5=SW 6=W 7=NW 8=CENTER 
+  // gamepad.SetHat(0, 8);
+
+
+  // Send all inputs via HID 
+  // Nothing is send to your computer until this is called.
+  gamepad.send_update();
+
+  // Flash the LED just for fun
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); 
+  delay(100);
 }
